6ad10333e78acafa8510151e3d47df49dce2d364
jeroen dijkmeijer <jeroen@stretchsketch.com>
2019-03-06 15:35:23 +0100

    step 3 completed

--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -1,8 +1,9 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+
 import scala.concurrent.duration._
 
 object Replicator {
@@ -15,10 +16,22 @@ object Replicator {
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
 }
 
-class Replicator(val replica: ActorRef) extends Actor {
+class Replicator(val replica: ActorRef) extends Actor with ActorLogging {
   import Replicator._
   import Replica._
   import context.dispatcher
+  var timer = FiniteDuration(100, TimeUnit.MILLISECONDS)
+  case class Check(val msg: String)
+
+  override def preStart(): Unit = {
+    super.preStart()
+    //implicit val ec = context.dispatcher
+    context.system.scheduler.schedule(
+      timer,
+      timer,
+      self,
+      new Check("everybody"))
+  }
   
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
@@ -36,10 +49,28 @@ class Replicator(val replica: ActorRef) extends Actor {
     ret
   }
 
+  def sendSnapshot(act: ActorRef, r: Replicate, seq: Long): Unit = {
+    val snapshot = Snapshot(r.key, r.valueOption, seq)
+    act ! snapshot
+    acks += ((seq) -> ((act, r)))
+  }
+
   
   /* TODO Behavior for the Replicator. */
   def receive: Receive = {
-    case _ =>
+    case r: Replicate => {
+      val snapshot = Snapshot(r.key, r.valueOption, nextSeq())
+      replica ! snapshot
+      acks += ((snapshot.seq , (sender,r)))}
+    case s: SnapshotAck => {
+      val tuple = acks(s.seq)
+      acks -= s.seq
+      tuple._1 ! Replicated(tuple._2.key, tuple._2.id)
+    }
+    case c: Check => {
+      log.warning("0,1 check")
+      acks.keys.foreach(key => { sendSnapshot(replica, acks(key)._2, key)})
+    }
   }
 
 }
